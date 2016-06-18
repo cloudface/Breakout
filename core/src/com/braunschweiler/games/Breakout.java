deab317e00b0943576b6d6387f2833b6ec272b0b
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2016-06-18 10:19:47 +0200

    Add ball and paddle

--- core/src/com/braunschweiler/games/Breakout.java
+++ core/src/com/braunschweiler/games/Breakout.java
@@ -7,34 +7,53 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
 public class Breakout extends ApplicationAdapter implements InputProcessor{
+	public static final int VIEWPORT_WIDTH = 800;
+	public static final int VIEWPORT_HEIGHT = 480;
+	public static final int BALL_SIZE = 64;
+	public static final int PADDLE_WIDTH = 64;
+	public static final int PADDLE_HEIGHT = 64;
+	public static final int INITIAL_BALL_VELOC_Y = -5;
+	public static final int MAX_INITIAL_BALL_VELOC_X = 5;
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
-	private Texture dropImage;
-	private Texture bucketImage;
-	private Rectangle bucket;
+	private Texture ballImage;
+	private Texture paddleImage;
+	private Rectangle ball;
+	private Rectangle paddle;
 	private Vector3 touchPos;
+	private int currentBallXVeloc;
+	private int currentBallYVeloc;
 
 	@Override
 	public void create () {
 		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
+		camera.setToOrtho(false, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 		batch = new SpriteBatch();
 
-		// load the images for the droplet and the bucket, 64x64 pixels each
-		dropImage = new Texture(Gdx.files.internal("droplet.png"));
-		bucketImage = new Texture(Gdx.files.internal("bucket.png"));
+		ballImage = new Texture(Gdx.files.internal("droplet.png"));
+		paddleImage = new Texture(Gdx.files.internal("bucket.png"));
 
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2;
-		bucket.y = 20;
-		bucket.width = 64;
-		bucket.height = 64;
+		ball = new Rectangle();
+		ball.x = VIEWPORT_WIDTH / 2 - BALL_SIZE / 2;
+		ball.y = VIEWPORT_HEIGHT - BALL_SIZE - 20;
+		ball.width = BALL_SIZE;
+		ball.height = BALL_SIZE;
+
+		paddle = new Rectangle();
+		paddle.x = VIEWPORT_WIDTH / 2 - PADDLE_WIDTH / 2;
+		paddle.y = 20;
+		paddle.width = PADDLE_WIDTH;
+		paddle.height = PADDLE_HEIGHT;
 
 		touchPos = new Vector3();
+
+		currentBallXVeloc = MathUtils.random(1, MAX_INITIAL_BALL_VELOC_X);
+		currentBallYVeloc = INITIAL_BALL_VELOC_Y;
 	}
 
 	@Override
@@ -43,16 +62,33 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
 		camera.update();
+		updateBall();
 
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
+		batch.draw(ballImage, ball.x, ball.y);
+		batch.draw(paddleImage, paddle.x, paddle.y);
 		batch.end();
 
 		if(Gdx.input.isTouched()) {
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
-			bucket.x = touchPos.x - 64 / 2;
+			paddle.x = touchPos.x - 64 / 2;
+		}
+	}
+
+	private void updateBall() {
+		ball.x += currentBallXVeloc;
+		ball.y += currentBallYVeloc;
+
+		if(ball.overlaps(paddle)){
+			currentBallYVeloc *= -1;
+		}
+		if(ball.x <= 0 || ball.x >= VIEWPORT_WIDTH){
+			currentBallXVeloc *= -1;
+		}
+		if(ball.y >= VIEWPORT_HEIGHT){
+			currentBallYVeloc *= -1;
 		}
 	}
 
