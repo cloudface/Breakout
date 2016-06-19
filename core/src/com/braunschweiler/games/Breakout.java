03b3b20fe2e5dbf2757395989a19bc4fb3c45aba
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2016-06-19 11:24:03 +0200

    Add bricks

--- core/src/com/braunschweiler/games/Breakout.java
+++ core/src/com/braunschweiler/games/Breakout.java
@@ -11,6 +11,11 @@ import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
 
+import org.w3c.dom.css.Rect;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Breakout extends ApplicationAdapter implements InputProcessor{
 	public static final int VIEWPORT_WIDTH = 800;
 	public static final int VIEWPORT_HEIGHT = 480;
@@ -19,15 +24,26 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 	public static final int PADDLE_HEIGHT = 64;
 	public static final int INITIAL_BALL_VELOC_Y = -5;
 	public static final int MAX_INITIAL_BALL_VELOC_X = 5;
+	public static final int TOTAL_NUMBER_OF_BRICKS = 20;
+	public static final int NUMBER_OF_BRICK_ROWS = 4;
+	public static final int NUMBER_OF_BRICKS_PER_ROW = TOTAL_NUMBER_OF_BRICKS / NUMBER_OF_BRICK_ROWS;
+	public static final int BRICK_AREA_HEIGHT = VIEWPORT_HEIGHT / 2 - 100;
+
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture ballImage;
 	private Texture paddleImage;
+	private Texture brickImage1;
+	private Texture brickImage2;
+	private Texture brickImage3;
+	private Texture brickImage4;
 	private Rectangle ball;
 	private Rectangle paddle;
+	private List<Rectangle> bricks;
 	private Vector3 touchPos;
 	private int currentBallXVeloc;
 	private int currentBallYVeloc;
+	List<Rectangle> bricksThatWereHit;
 
 	@Override
 	public void create () {
@@ -37,10 +53,14 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 
 		ballImage = new Texture(Gdx.files.internal("droplet.png"));
 		paddleImage = new Texture(Gdx.files.internal("bucket.png"));
+		brickImage1 = new Texture(Gdx.files.internal("block1.png"));
+		brickImage2 = new Texture(Gdx.files.internal("block2.png"));
+		brickImage3 = new Texture(Gdx.files.internal("block3.png"));
+		brickImage4 = new Texture(Gdx.files.internal("block4.png"));
 
 		ball = new Rectangle();
 		ball.x = VIEWPORT_WIDTH / 2 - BALL_SIZE / 2;
-		ball.y = VIEWPORT_HEIGHT - BALL_SIZE - 20;
+		ball.y = BRICK_AREA_HEIGHT - BALL_SIZE - 50;
 		ball.width = BALL_SIZE;
 		ball.height = BALL_SIZE;
 
@@ -54,6 +74,25 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 
 		currentBallXVeloc = MathUtils.random(1, MAX_INITIAL_BALL_VELOC_X);
 		currentBallYVeloc = INITIAL_BALL_VELOC_Y;
+
+		bricks = new ArrayList<Rectangle>();
+		bricksThatWereHit = new ArrayList<Rectangle>();
+		initializeBricks();
+	}
+
+	private void initializeBricks() {
+		int brickWidth = VIEWPORT_WIDTH / NUMBER_OF_BRICKS_PER_ROW;
+		int brickHeight = BRICK_AREA_HEIGHT / NUMBER_OF_BRICK_ROWS;
+		for(int i = 0; i < NUMBER_OF_BRICK_ROWS; i++){
+			for(int j = 0; j < NUMBER_OF_BRICKS_PER_ROW; j++){
+				Rectangle brick = new Rectangle();
+				brick.x = j * brickWidth;
+				brick.y = VIEWPORT_HEIGHT - brickHeight - (i * brickHeight);
+				brick.width = brickWidth;
+				brick.height = brickHeight;
+				bricks.add(brick);
+			}
+		}
 	}
 
 	@Override
@@ -64,12 +103,23 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 		camera.update();
 		updateBall();
 
+		drawScene();
+
+		updatePaddleBasedOnUserInput();
+	}
+
+	private void drawScene() {
 		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 		batch.draw(ballImage, ball.x, ball.y);
 		batch.draw(paddleImage, paddle.x, paddle.y);
+		for(Rectangle brick : bricks){
+			batch.draw(brickImage1, brick.x, brick.y, brick.width, brick.height);
+		}
 		batch.end();
+	}
 
+	private void updatePaddleBasedOnUserInput() {
 		if(Gdx.input.isTouched()) {
 			touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
 			camera.unproject(touchPos);
@@ -90,6 +140,25 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 		if(ball.y >= VIEWPORT_HEIGHT){
 			currentBallYVeloc *= -1;
 		}
+
+		updateBricks();
+	}
+
+	private void updateBricks() {
+		bricksThatWereHit.clear();
+		for(Rectangle brick : bricks){
+			if(ball.overlaps(brick)){
+				currentBallYVeloc *= -1;
+				bricksThatWereHit.add(brick);
+			}
+		}
+		for(Rectangle brick : bricksThatWereHit){
+			destroyBrick(brick);
+		}
+	}
+
+	private void destroyBrick(Rectangle brick) {
+		bricks.remove(brick);
 	}
 
 	@Override
