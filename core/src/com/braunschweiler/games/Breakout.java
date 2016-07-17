de534dd9d0c8aba68440d96c5c624733cb088bfd
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2016-07-17 15:15:40 +0200

    Ball reverses direction if it collides with specific collision zones of paddle

--- core/src/com/braunschweiler/games/Breakout.java
+++ core/src/com/braunschweiler/games/Breakout.java
@@ -15,10 +15,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Breakout extends ApplicationAdapter implements InputProcessor{
+	private static final String LOG_TAG = Breakout.class.getSimpleName();
 	public static final int VIEWPORT_WIDTH = 800;
 	public static final int VIEWPORT_HEIGHT = 480;
 	public static final int BALL_SIZE = 64;
-	public static final int PADDLE_WIDTH = 64;
+	public static final int PADDLE_WIDTH = 256;
 	public static final int PADDLE_HEIGHT = 64;
 	public static final int INITIAL_BALL_VELOC_Y = -5;
 	public static final int MAX_INITIAL_BALL_VELOC_X = 5;
@@ -26,6 +27,8 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 	public static final int NUMBER_OF_BRICK_ROWS = 4;
 	public static final int NUMBER_OF_BRICKS_PER_ROW = TOTAL_NUMBER_OF_BRICKS / NUMBER_OF_BRICK_ROWS;
 	public static final int BRICK_AREA_HEIGHT = VIEWPORT_HEIGHT / 2 - 100;
+	public static final int PADDLE_NEUTRAL_COLLISION_ZONE = PADDLE_WIDTH /8;
+	public static final int COLLISION_ZONE_X_VELOC = 5;
 
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
@@ -49,8 +52,8 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 		camera.setToOrtho(false, VIEWPORT_WIDTH, VIEWPORT_HEIGHT);
 		batch = new SpriteBatch();
 
-		ballImage = new Texture(Gdx.files.internal("droplet.png"));
-		paddleImage = new Texture(Gdx.files.internal("bucket.png"));
+		ballImage = new Texture(Gdx.files.internal("ball.png"));
+		paddleImage = new Texture(Gdx.files.internal("paddle.png"));
 		brickImage1 = new Texture(Gdx.files.internal("block1.png"));
 		touchPos = new Vector3();
 
@@ -147,7 +150,21 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 		ball.y += currentBallYVeloc;
 
 		if(ball.overlaps(paddle)){
-			updateBallVelocityBasedOnCollision(paddle);
+			CollisionType collisionType = updateBallVelocityBasedOnCollision(paddle);
+			if(collisionType == CollisionType.CollidesWithTopOrBottom){
+				int paddleCenter = (int) (paddle.x + (paddle.width / 2));
+				int paddleLeftCollisionZone = paddleCenter - PADDLE_NEUTRAL_COLLISION_ZONE;
+				int paddleRightCollisionZone = paddleCenter + PADDLE_NEUTRAL_COLLISION_ZONE;
+				if((ball.x + ball.width) <= paddleLeftCollisionZone){
+					//We are colliding with the left part of the top of the paddle. Give ball
+					//a certain X velocity
+					currentBallXVeloc = -COLLISION_ZONE_X_VELOC;
+				} else if(ball.x >= paddleRightCollisionZone){
+					//We are colliding with the right part of the top of the paddle. Give ball
+					//a certain X velocity
+					currentBallXVeloc = COLLISION_ZONE_X_VELOC;
+				}
+			}
 		}
 		if(ball.x <= 0 || ball.x >= VIEWPORT_WIDTH){
 			currentBallXVeloc *= -1;
@@ -173,7 +190,8 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 		}
 	}
 
-	private void updateBallVelocityBasedOnCollision(Rectangle entity) {
+	private CollisionType updateBallVelocityBasedOnCollision(Rectangle entity) {
+		CollisionType collisionType;
 		float xIntrusion;
 		float yIntrusion;
 		if(currentBallXVeloc > 0){
@@ -194,14 +212,19 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 		if(xIntrusion < yIntrusion){
             //Ball is colliding with the sides of the entity
             currentBallXVeloc *= -1;
+			collisionType = CollisionType.CollidesWithSides;
         } else if(yIntrusion < xIntrusion){
             //Ball is colliding with the top or bottom of the entity
             currentBallYVeloc *= -1;
+			collisionType = CollisionType.CollidesWithTopOrBottom;
         } else {
             //Ball is colliding perfectly with one of the corners of the entity
             currentBallXVeloc *= -1;
             currentBallYVeloc *= -1;
+			collisionType = CollisionType.CollidesWithCorner;
         }
+
+		return collisionType;
 	}
 
 	private void destroyBrick(Rectangle brick) {
@@ -247,5 +270,11 @@ public class Breakout extends ApplicationAdapter implements InputProcessor{
 	@Override
 	public boolean scrolled(int amount) {
 		return false;
+	}
+
+	public enum CollisionType{
+		CollidesWithTopOrBottom,
+		CollidesWithSides,
+		CollidesWithCorner
 	}
 }
